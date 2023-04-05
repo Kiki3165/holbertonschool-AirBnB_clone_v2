7858b64c9d7990585c3c94eb73aac25333dbc3a1
kiki3165 <kikikou82@gmail.com>
2023-04-05 13:34:45 +0000

    task0

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,13 +5,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
-    """docu"""
 def route()
+"""docu"""
     return 'Hello HBNB!'
 
 @app.route('/hbnb', strict_slashes=False)
-    """docu"""
 def routeX()
+"""docu"""
     return 'HBNB'
 
 if __name__ == '__main__':
