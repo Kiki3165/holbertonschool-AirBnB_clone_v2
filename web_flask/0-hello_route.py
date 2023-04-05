daf3688918ec9e679347f4d378b21463971ad0bb
kiki3165 <kikikou82@gmail.com>
2023-04-05 13:08:49 +0000

    doc

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-'''script'''
+#docu
 
 from flask import Flask
 
-'''doc'''
+# docu
 
 app = Flask(__name__)
 
+# docu
+
 @app.route('/', strict_slashes=False')
-    '''def route'''
+    # def route
     def route()
         return 'Hello HBNB!'
 
-'''module'''
+# module
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
