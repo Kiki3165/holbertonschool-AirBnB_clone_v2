6808222f001af4105cc02a03b972ed71a985eae0
kiki3165 <kikikou82@gmail.com>
2023-04-05 13:39:20 +0000

    task0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,10 +4,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False')
-           """docu"""
-def route()
-     return 'Hello HBNB!'
+@app.route('/', strict_slashes=False)
+def route():
+    """docu"""
+    return 'Hello HBNB!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
