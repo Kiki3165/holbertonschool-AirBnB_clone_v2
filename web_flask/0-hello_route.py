8b4848d1825e159be60baf9eb37128cece12bc13
kiki3165 <kikikou82@gmail.com>
2023-04-05 13:00:19 +0000

    first commit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,10 +4,10 @@
 from flask import Flask
 
 app = Flask(__name__)
-
+'''doc'''
 @app.route('/')
 def route()
     return 'Hello HBNB!'
-
+'''module'''
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
