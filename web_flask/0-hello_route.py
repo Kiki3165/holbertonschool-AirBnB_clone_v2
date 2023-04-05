02cd566d0baae9632ef4551897c35c51faaf7ea7
kiki3165 <kikikou82@gmail.com>
2023-04-05 13:13:45 +0000

    test

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,18 +3,11 @@
 
 from flask import Flask
 
-"""docu"""
-
 app = Flask(__name__)
 
-"""docu"""
-
 @app.route('/', strict_slashes=False')
-    """def route"""
     def route()
         return 'Hello HBNB!'
-
-"""docu"""
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
