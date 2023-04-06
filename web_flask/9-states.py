a0fa71593aee71d469d785100a0e33cfb811754e
kiki3165 <kikikou82@gmail.com>
2023-04-06 20:24:39 +0000

    change code

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -11,28 +11,28 @@ app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def close_db(error):
-    """Close the database"""
+def close(exception):
     storage.close()
 
 
-@app.route('/states', strict_slashes=False)
-def states():
-    """Display a list of all states"""
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda state: state.name)
-    return render_template('9-states.html', states=states)
+@app.route("/states", strict_slashes=False)
+def listStates():
+    States = storage.all(State)
+    Cities = storage.all(City)
+
+    return render_template("9-states.html", states=States, cities=Cities)
 
 
-@app.route('/states/<id>', strict_slashes=False)
-def state_cities(id):
-    """Display a list of all cities in a given state"""
-    state = storage.get(State, id)
-    if state is None:
-        return render_template('9-not_found.html'), 404
-    cities = sorted(state.cities, key=lambda city: city.name)
-    return render_template('9-state.html', state=state, cities=cities)
+@app.route("/states/<id>", strict_slashes=False)
+def listStateCities(id):
+    Citis = storage.all(City)
+    StatesList = storage.all(State)
+    for state in StatesList.values():
+        print(str(state.id) + " ===" + str(id))
+        if str(state.id) == str(id):
+            return render_template("9-states.html", states=state, cities=Citis)
+    return render_template("9-states.html", cities=Citis)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
