d9893a8a8ae77979f550172e974d8e3362c61779
kiki3165 <kikikou82@gmail.com>
2023-04-06 15:20:25 +0000

    task8 with template

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""docu"""
+
+from models import storage
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_storage(exception):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def show_states_list():
+    """Display a list of all State objects sorted by name"""
+    states = storage.all('State')
+    states_sorted = sorted(states.values(), key=lambda s: s.name)
+    return render_template('states_list.html', states=states_sorted)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
