d86ef79bed3750b8bae05ddea6c17daf18fe350f
kiki3165 <kikikou82@gmail.com>
2023-04-06 15:50:11 +0000

    task8 with template

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -2,25 +2,30 @@
 """doc"""
 
 from flask import Flask, render_template
-from models import storage, State
+from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """ Remove the current SQLAlchemy Session """
+def teardown_session(exception):
+    """
+    Remove the current SQLAlchemy Session after each request.
+    """
     storage.close()
 
 
 @app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    """ Display a HTML page with all cities by state """
+def states_cities_list():
+    """
+    Displays a list of states and cities.
+    """
     states = storage.all(State).values()
-    states_sorted = sorted(states, key=lambda x: x.name)
+    states = sorted(states, key=lambda state: state.name)
 
-    return render_template('8-cities_by_states.html', states=states_sorted)
+    return render_template('8-cities_by_states.html', states=states)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
